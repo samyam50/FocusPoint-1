fec6871c2f617f10d0d5ff23bc4919ce1100d499
Samyam Ghimire <samyam50@gmail.com>
2022-03-15 13:17:21 +1100

    Page creation

--- components/goals.js
+++ components/goals.js
@@ -2,17 +2,10 @@ import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import home from './components/home';
-import focus from './components/focus';
-import todo from './components/todo';
-import work from './components/work';
-import goals from './components/goals';
-
-
-export default function App() {
+export default function goals() {
   return (
     <View style={styles.container}>
-      <Text>hi mate</Text>
+      <Text>goals page</Text>
       <StatusBar style="auto" />
     </View>
   );
--- components/todo.js
+++ components/todo.js
@@ -2,14 +2,7 @@ import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import home from './components/home';
-import focus from './components/focus';
-import todo from './components/todo';
-import work from './components/work';
-import goals from './components/goals';
-
-
-export default function App() {
+export default function todo() {
   return (
     <View style={styles.container}>
       <Text>hi mate</Text>
